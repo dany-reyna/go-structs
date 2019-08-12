77a6beb04da687d631b505358d4a78691c1e934e
Dany Reyna <luisd.reyna@outlook.com>
2019-08-11 21:28:41 -0500

    Structs

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	alex := person{firstName: "Alex", lastName: "Anderson"}
+	fmt.Println(alex)
+
+	var pepe person
+
+	pepe.firstName = "Pepe"
+	pepe.lastName = "Anderson"
+
+	fmt.Println(pepe)
+	fmt.Printf("%+v\n", pepe)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
